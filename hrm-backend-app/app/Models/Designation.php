509e22097da5c1d31cfa054c89628043df927357
Abdullah Al Mamun <abdullah.mamun2528@gmail.com>
2025-12-01 12:11:17 +0600

    Update Department and Designation models and migrations to use ULIDs

--- hrm-backend-app/app/Models/Designation.php
+++ hrm-backend-app/app/Models/Designation.php
@@ -4,12 +4,27 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 
 class Designation extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
 
+    public $incrementing = false;
+    protected $keyType = 'string';
     protected $fillable = ['title'];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->id)) {
+                $model->id = Str::ulid()->toBase32();
+            }
+        });
+    }
 
     public function employees()
     {
